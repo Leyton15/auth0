e515b0e0feb44101fa148891a82bdb95d80d8e44
Leyton15 <lveintimi1@utmachala.edu.ec>
2024-06-04 18:23:44 -0500

    copilot
    Co-authored-by: Paul Zambrano <paulzamm@users.noreply.github.com>

--- src/app/Services/servicio.service.ts
+++ src/app/Services/servicio.service.ts
@@ -4,6 +4,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ServicioService {
+  carros:any[] = [
+    {
+      id:1
+    }
+  ];
 
-  constructor() { }
+    constructor() { }
 }
